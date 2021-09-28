d9aa034f5ad6988d61c6326e4bce66365e558272
run <gigimugitya0852@gmail.com>
2021-09-28 20:55:44 +0900

    first commit

---
+++ pandas.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+
+import streamlit as st
+import pandas as pd
+
+def main():
+
+    st.table(pd.DataFrame({
+        'first column': [10, 20, 30, 40],
+        'second column': [100, 200, 300, 400]
+    }))
+
+if __name__ == '__main__':
+    main()
